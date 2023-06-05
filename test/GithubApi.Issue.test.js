b3aa26fd345c2b34ffd117a89dc613a38a64ae7f
Dtorres1ro <46894535+Datorresp@users.noreply.github.com>
2023-06-05 17:47:18 -0500

    Update GithubApi.Issue.test.js

--- test/GithubApi.Issue.test.js
+++ test/GithubApi.Issue.test.js
@@ -1,7 +1,6 @@
 const { StatusCodes } = require('http-status-codes');
 const { expect } = require('chai');
 const axios = require('axios');
-const dotenv = require("dotenv");
 
 const urlBase = 'https://api.github.com/user';
 const githubUserName = 'Datorresp';
@@ -22,24 +21,18 @@ async function getIssue() {
 
 describe('Github Api Testing', function () {
 
-
-    beforeEach(() => {
-
-        dotenv.config();
-    });
-
     describe('create Issues', function () {
 
         it("CREATE", async () => {
 
             const issue = {
             title: `${issueTitle}`,
-            owner: `${username}`,
+            owner: `${githubUserName}`,
             repo: `${repository}`,
         };
 
         const issuePostResponse = await axios.post(
-            `${urlBase}/repos/${username}/${repository}/issues`,
+            `${urlBase}/repos/${githubUserName}/${repository}/issues`,
             issue,
             {
                 headers: {
@@ -49,94 +42,80 @@ describe('Github Api Testing', function () {
             }
         );
 
-        expect(issuePostResponse.status).to.equal(StatusCodes.CREATED);
-        expect(issuePostResponse.data.title).to.equal(issue.title);
-        expect(issuePostResponse.data.body).not.exist;
+        assert.strictEqual(response.status, 201);
+        issueNumber = response.data.number;
+        assert.strictEqual(response.data.title, issueTitle);
+        assert.strictEqual(response.data.body, null);
         issueNumber = issuePostResponse.data.number;
         });
 
         it('Update an issue', async function () {
 
             const issue = {
-                  owner: `${username}`,
+                  owner: `${githubUserName}`,
                   repo: `${repository}`,
                   issue_number: `${issueNumber}`,
                   body: "Holaaaa",
-                };
+            };
 
                 const issuePatchResponse = await axios.patch(
-                  `${urlBase}/repos/${username}/${repository}/issues/${issueNumber}`,
-                  issue,
-                  {
-                    headers: {
-                      Authorization: `token ${process.env.ACCESS_TOKEN}`,
-                    },
-                  }
+                    `${urlBase}/repos/${githubUserName}/${repository}/issues/${issueNumber}`,
+                    issue,
+                    {
+                        headers: {
+                         Authorization: `token ${process.env.ACCESS_TOKEN}`,
+                        },
+                    }
                 );
 
-                expect(issuePatchResponse.status).to.equal(StatusCodes.OK);
-
-                const issueGetResponse = await getIssue();
-
-                expect(issueGetResponse.data.title).to.equal(`${issueTitle}`);
-                expect(issueGetResponse.data.body).to.equal(issue.body);
+                assert.strictEqual(response.status, 200);
+                assert.strictEqual(response.data.number, issueNumber);
+                assert.strictEqual(response.data.title, issueTitle);
+                assert.strictEqual(response.data.body, issueBody);
         });
 
-        it('Lock an issue', async function () {
+        it("Lock issue", async () => {
+            const issue = {
+                owner: `${githubUserName}`,
+                repo: `${repository}`,
+                issue_number: `${issueNumber}`,
+                lock_reason: "resolved",
+            };
 
-            const url = `https://api.github.com/repos/${username}/${repositoryName}/issues/${issueNumber}/lock`;
-            const response = await axios.put(url,
-            {
-              lock_reason: 'resolved',
-            },
+            const putLockResponse = await axios.put(
+                `${urlBase}/repos/${githubUserName}/${repository}/issues/${issueNumber}/lock`,
+                issue,
                 {
                     headers: {
-                        Authorization: `token ${process.env.ACCESS_TOKEN}`,
+                    Authorization: `token ${process.env.ACCESS_TOKEN}`,
                     },
                 }
             );
+
             assert.strictEqual(response.status, 204);
-            assert.strictEqual(response.data.locked, true);
-            assert.strictEqual(response.data.active_lock_reason, 'resolved');
-        });
-
-        it('Lock Issue', async function () {
-
-            const issue = {
-                  owner: `${username}`,
-                  repo: `${repository}`,
-                  issue_number: `${issueNumber}`,
-                  lock_reason: "resolved",
-            };
-
-            const url = `https://api.github.com/repos/${username}/${repositoryName}/issues/${issueNumber}`;
-            const response = await axios.get(url, issue,
-                {
-                    headers: {
-                        Authorization: `token ${process.env.ACCESS_TOKEN}`,
-                    },
-                }
-            });
-            assert.strictEqual(response.status, 200);
             assert.strictEqual(response.data.locked, true);
             assert.strictEqual(response.data.active_lock_reason, 'resolved');
         });
 
         it("Unlock issue", async () => {
             const issue = {
-              owner: `${username}`,
+              owner: `${githubUserName}`,
               repo: `${repository}`,
               issue_number: `${issueNumber}`,
             };
 
             const deleteUnlockResponse = await axios.delete(
-                    `${urlBase}/repos/${username}/${repository}/issues/${issueNumber}/lock`,
+                    `${urlBase}/repos/${githubUserName}/${repository}/issues/${issueNumber}/lock`,
                 {
                     headers: {
                         Authorization: `token ${process.env.ACCESS_TOKEN}`,
                     },
                 }
             );
+
+            assert.strictEqual(response.status, 204);
+            assert.strictEqual(response.data.locked, false);
+            assert.strictEqual(response.data.active_lock_reason, null);
         });
     });
-}
+});
